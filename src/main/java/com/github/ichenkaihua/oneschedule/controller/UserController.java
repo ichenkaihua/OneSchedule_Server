7bf946d1c9c7b2c67ceb3b30b529e3a4b6a28483
ichenkaihua <admin@chenkaihua.com>
2016-03-20 17:18:58 +0800

    修改接口

--- src/main/java/com/github/ichenkaihua/oneschedule/controller/UserController.java
+++ src/main/java/com/github/ichenkaihua/oneschedule/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.github.ichenkaihua.oneschedule.controller;
 
+import com.github.ichenkaihua.oneschedule.constance.Status;
 import com.github.ichenkaihua.oneschedule.entity.User;
 import com.github.ichenkaihua.oneschedule.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by chenkh on 16-3-20.
@@ -20,7 +21,42 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("{id}")
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity register(@RequestParam String phone, @RequestParam String password) {
+        if (!userService.hasRegister(phone)) {
+            User user = userService.addUser(phone, password);
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(Status.STATUS_1).build();
+        }
+
+    }
+
+
+    /**
+     * 登陆用户
+     * @param phone 电话号码
+     * @param password 密码
+     * @return
+     */
+    public ResponseEntity login(@RequestParam String phone, String password) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setPassword(password);
+        if (userService.hasRegister(phone)) {
+            return ResponseEntity.status(Status.STATUS_1).build();
+        } else {
+            List<User> select = userService.select(user);
+            if (select == null || select.isEmpty()) {
+                return ResponseEntity.status(Status.STATUS_2).build();
+            }
+            return ResponseEntity.ok(select.get(0));
+        }
+    }
+
+
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable int id) {
         return userService.getUserById(id);
     }
--- src/main/java/com/github/ichenkaihua/oneschedule/service/UserService.java
+++ src/main/java/com/github/ichenkaihua/oneschedule/service/UserService.java
@@ -2,26 +2,40 @@ package com.github.ichenkaihua.oneschedule.service;
 
 import com.github.ichenkaihua.oneschedule.entity.User;
 import com.github.ichenkaihua.oneschedule.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * Created by chenkh on 16-3-20.
  */
 @Service
-public class UserService {
-
-    @Autowired
-    UserMapper userMapper;
+public class UserService extends BaseService<UserMapper,User> {
 
 
 
-    public User getUserById(int id){
-        return userMapper.selectByPrimaryKey(id);
-
+    public User getUserById(int id) {
+        return selectByPrimaryKey(id);
     }
 
 
+    public boolean hasRegister(String phone){
+        User queryUser = new User();
+        queryUser.setPhone(phone);
+        return selectCount(queryUser)>0;
+    }
+
+
+    public User addUser(String phone, String password) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setPassword(password);
+        Date date = new Date();
+        user.setCreateTime(date);
+        user.setLastTime(date);
+        mapper.insert(user);
+        return user;
+    }
 
 
 }
